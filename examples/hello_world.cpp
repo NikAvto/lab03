c9a6912b11670dce7ae7aeaf0f79332ae890e085
NikAvto <146371520+NikAvto@users.noreply.github.com>
2024-02-26 10:47:02 +0300

    Update hello_world.cpp

--- examples/hello_world.cpp
+++ examples/hello_world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main() {
-	// Fixed compilation errors
+	// Fixed compilation errors (not)
 	std::string name;
 	std::cin >> name;
 	std::cout << "Hello world from " << name << std::endl;
